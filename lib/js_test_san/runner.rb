eabd184ffbbdb6799b67cac1457c3e94029ca828
Eloy Duran <eloy.de.enige@gmail.com>
2009-04-11 21:41:45 +0200

    Took first stab at JSTestSan::Runner

--- lib/js_test_san/runner.rb
+++ lib/js_test_san/runner.rb
@@ -1,12 +1,43 @@
+require 'osx/cocoa'
+
 module JSTestSan
-  class Runner
-    attr_reader :html_files
+  class Runner < OSX::NSObject
+    attr_reader :test_cases, :running
     
-    def initialize(html_files)
-      @html_files = html_files
-      @html_files.each do |file|
-        raise FileDoesNotExistError, "The file `#{file}' does not exist." unless File.exist?(file)
+    def initWithHTMLFiles(html_files)
+      if init
+        @test_cases = []
+        @running = []
+        
+        html_files.each do |file|
+          @test_cases << TestCase.alloc.initWithHTMLFile_delegate(file, self)
+        end
+        
+        self
       end
+    end
+    
+    def run
+      @test_cases.each do |test_case|
+        test_case.run
+        @running << test_case
+      end
+      OSX::NSApplication.sharedApplication.run
+    end
+    
+    def tests;      sum :tests      end
+    def assertions; sum :assertions end
+    def failures;   sum :failures   end
+    def errors;     sum :errors     end
+    
+    def test_case_finished(test_case)
+      @running.delete(test_case)
+    end
+    
+    private
+    
+    def sum(attr)
+      @test_cases.inject(0) { |sum, tc| sum += tc.send(attr) }
     end
   end
 end
--- test/unit/runner_test.rb
+++ test/unit/runner_test.rb
@@ -2,25 +2,59 @@ require File.expand_path('../../test_helper', __FILE__)
 
 describe "JSTestSan::Runner" do
   before do
-    @runner = JSTestSan::Runner.new([fixture('a_unit_test.html')])
-  end
-  
-  it "should initialize with an array of html files" do
-    @runner.html_files.should == [fixture('a_unit_test.html')]
-  end
-  
-  it "should raise a FileDoesNotExistError if a given html file does not exist" do
-    lambda {
-      JSTestSan::Runner.new %w{ /does/not/exist/test.html }
-    }.should.raise JSTestSan::FileDoesNotExistError
+    files = [fixture('a_unit_test.html'), fixture('a_unit_test.html')]
+    @runner = JSTestSan::Runner.alloc.initWithHTMLFiles(files)
+    
+    @runner.test_cases.first.stubs(:run)
+    @runner.test_cases.last.stubs(:run)
+    OSX::NSApplication.sharedApplication.stubs(:run)
   end
   
   it "should have instantiated a JSTestSan::TestCase for each html file" do
-    @runner.test_cases.should == JSTestSan::TestCase.new()
+    @runner.test_cases.length.should == 2
+    @runner.test_cases.each do |test_case|
+      test_case.html_file.should == fixture('a_unit_test.html')
+      test_case.delegate.should == @runner
+    end
   end
   
-  xit "should run the tests" do
-    @runner.expects(:passed).with()
+  it "should run the tests cases and start a runloop" do
+    @runner.test_cases.first.expects(:run)
+    OSX::NSApplication.sharedApplication.expects(:run)
     @runner.run
+  end
+  
+  it "should keep a `running' stack which contains all test cases that are running" do
+    @runner.run
+    @runner.running.should == @runner.test_cases
+  end
+  
+  it "should remove a finished test case from the `running' stack" do
+    @runner.run
+    @runner.running.should.not.be.empty
+    @runner.test_case_finished(@runner.test_cases.first)
+    @runner.running.should.not.be.empty
+    @runner.test_case_finished(@runner.test_cases.last)
+    @runner.running.should.be.empty
+  end
+  
+  it "should return the total amount of tests" do
+    @runner.test_cases.each { |tc| tc.stubs(:tests).returns(3) }
+    @runner.tests.should == 6
+  end
+  
+  it "should return the total amount of assertions" do
+    @runner.test_cases.each { |tc| tc.stubs(:assertions).returns(2) }
+    @runner.assertions.should == 4
+  end
+  
+  it "should return the total amount of failures" do
+    @runner.test_cases.each { |tc| tc.stubs(:failures).returns(1) }
+    @runner.failures.should == 2
+  end
+  
+  it "should return the total amount of errors" do
+    @runner.test_cases.each { |tc| tc.stubs(:errors).returns(1) }
+    @runner.errors.should == 2
   end
 end
